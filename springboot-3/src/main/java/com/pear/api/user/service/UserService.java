a94624d55a623a5ec68e0dbacabb31de048e93b0
GiselePark <hanied04@gmail.com>
2024-05-16 23:39:52 +0900

    Rename

--- springboot-3/src/main/java/com/pear/api/user/service/UserService.java
+++ springboot-3/src/main/java/com/pear/api/user/service/UserService.java
@@ -3,14 +3,14 @@ package com.pear.api.user.service;
 import com.pear.api.common.component.Messenger;
 import com.pear.api.common.service.CommandService;
 import com.pear.api.common.service.QueryService;
+import com.pear.api.user.model.User;
 import com.pear.api.user.model.UserDTO;
-import com.pear.api.user.model.UserEntity;
 
 public interface UserService extends CommandService<UserDTO>, QueryService<UserDTO> {
     Messenger modify(UserDTO user);
 
-    default UserEntity dtoToEntity(UserDTO dto) {
-        return UserEntity.builder()
+    default User dtoToEntity(UserDTO dto) {
+        return User.builder()
                 .id(dto.getId())
                 .username(dto.getUsername())
                 .password(dto.getPassword())
@@ -21,7 +21,7 @@ public interface UserService extends CommandService<UserDTO>, QueryService<UserD
                 .build();
     }
 
-    default UserDTO entityToDto(UserEntity user) {
+    default UserDTO entityToDto(User user) {
         return UserDTO.builder()
                 .id(user.getId())
                 .username(user.getUsername())
