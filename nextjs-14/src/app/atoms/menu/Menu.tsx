7110200155d02c1a3dc682ab5e9d155263afeebf
GiselePark <hanied04@gmail.com>
2024-05-24 20:18:15 +0900

    Design - add toggle

---
+++ nextjs-14/src/app/atoms/menu/Menu.tsx
@@ -0,0 +1,11 @@
+export function Menu() {
+  return (
+    <nav className="menu">
+      <ul>
+        <li>sign in</li>
+        <li>sign up</li>
+        <li>my page</li>
+      </ul>
+    </nav>
+  );
+}
