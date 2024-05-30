b73654533792dfa688dfb2c5570a40ea55298bf1
GiselePark <hanied04@gmail.com>
2024-05-30 10:00:50 +0900

    Feat - Payment repository

---
+++ springboot-3/src/main/java/com/pear/api/payment/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.pear.api.payment.repository;
+
+import com.pear.api.payment.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
