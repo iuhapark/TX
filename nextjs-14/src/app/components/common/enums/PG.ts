fca615b7c043ef4d58d26a3546bc753c08e72865
GiselePark <hanied04@gmail.com>
2024-05-31 01:38:02 +0900

    Feat - add path

--- nextjs-14/src/app/components/common/enums/PG.ts
+++ nextjs-14/src/app/components/common/enums/PG.ts
@@ -1,9 +1,14 @@
 export const enum PG {
+  // user
   USER = "/pages/users",
+  DETAIL = `/detail`,
+
   BOARD = "/pages/boards",
   POST = "/pages//posts",
+
   DEMO = "/pages/demos",
   ABOUT = "/pages/about",
+  
   PAY = "/pages/payments",
   ITEM = "/pages/products"
 }
