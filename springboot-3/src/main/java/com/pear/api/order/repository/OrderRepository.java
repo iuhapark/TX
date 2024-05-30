bbe1ca169764a41a5c5e64e86c56fbe4ffc7afde
GiselePark <hanied04@gmail.com>
2024-05-30 09:59:31 +0900

    Feat - JPQL Query

---
+++ springboot-3/src/main/java/com/pear/api/order/repository/OrderRepository.java
@@ -0,0 +1,22 @@
+package com.pear.api.order.repository;
+
+import com.pear.api.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("select o from Order o" +
+            " left join fetch o.payment p" +
+            " left join fetch o.user m" +
+            " where o.orderUid = :orderUid")
+    Optional<Order> findOrderAndPaymentAndMember(String orderUid);
+
+    @Query("select o from Order o" +
+            " left join fetch o.payment p" +
+            " where o.orderUid = :orderUid")
+    Optional<Order> findOrderAndPayment(String orderUid);
+}
