564cba58885d80aea2270a99582bd1577f8d68cd
GiselePark <hanied04@gmail.com>
2024-05-31 01:34:37 +0900

    Feat - test CorsFilter

---
+++ springboot-3/src/main/java/com/pear/api/common/config/CorsFilter.java
@@ -0,0 +1,14 @@
+//package com.pear.api.common.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//
+//@Configuration
+//@EnableWebFlux
+//public class CorsFilter implements WebFluxConfigurer {
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("*");
+//    }
+//}
