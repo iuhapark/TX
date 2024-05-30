0153c99d7e6bb7893c2113be19f29e4f962f14cc
GiselePark <hanied04@gmail.com>
2024-05-30 10:05:11 +0900

    Feat - implement test methods, add builder

---
+++ springboot-3/src/main/java/com/pear/api/order/service/OrderServiceImpl.java
@@ -0,0 +1,41 @@
+package com.pear.api.order.service;
+
+import com.pear.api.common.emums.PaymentStatus;
+import com.pear.api.order.model.Order;
+import com.pear.api.order.repository.OrderRepository;
+import com.pear.api.payment.model.Payment;
+import com.pear.api.payment.repository.PaymentRepository;
+import com.pear.api.user.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService{
+    private final OrderRepository orderRepository;
+    private final PaymentRepository paymentRepository;
+
+    public Order autoOrder(User user) {
+        // 임시 결제내역 생성
+        Payment payment = Payment.builder()
+                .price(1000L)
+                .status(PaymentStatus.READY)
+                .build();
+        paymentRepository.save(payment);
+
+        // 주문 생성
+        Order order = Order.builder()
+                .user(user)
+                .price(1000L)
+                .itemName("1달러샵 상품")
+                .orderUid(UUID.randomUUID().toString())
+                .payment(payment)
+                .build();
+
+        return orderRepository.save(order);
+    }
+}
