f3fb71f1b31da8e97d7dccd7552d753d7bf95b5b
GiselePark <hanied04@gmail.com>
2024-05-30 10:11:11 +0900

    Feat - contract for CRUD

--- springboot-3/src/main/java/com/pear/api/common/service/QueryService.java
+++ springboot-3/src/main/java/com/pear/api/common/service/QueryService.java
@@ -1,8 +1,13 @@
 package com.pear.api.common.service;
 
+import com.pear.api.common.component.Messenger;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface QueryService<T> {
     List<T> findAll() ;
+    Optional<T> findById(Long id);
+    Messenger count();
     boolean existsById(Long id);
 }
