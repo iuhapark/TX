dd588ea057d4eaee0fa24c5739f3c929406e6aee
GiselePark <hanied04@gmail.com>
2024-06-03 22:18:03 +0900

    Feat - jwt-decode

--- nextjs-14/src/app/pages/users/login/page.tsx
+++ nextjs-14/src/app/pages/users/login/page.tsx
@@ -1,166 +1,170 @@
-'use client'
+"use client";
 
 import Link from "next/link";
-import React, { useState, useEffect, useRef } from "react"
-import { useSelector, useDispatch } from 'react-redux'
-import { useRouter } from "next/navigation"
-import { existsUsername, login } from "@/app/components/user/service/user-service";
+import React, { useState, useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useRouter } from "next/navigation";
+import {
+  existsUsername,
+  login,
+} from "@/app/components/user/service/user-service";
 import { IUser } from "@/app/components/user/model/user";
-import nookies, { parseCookies, destroyCookie, setCookie } from 'nookies'
-import { getAuth, getExistsUsername } from "@/app/components/user/service/user-slice";
+import nookies, { parseCookies, destroyCookie, setCookie } from "nookies";
+import {
+  getAuth,
+  getExistsUsername,
+} from "@/app/components/user/service/user-slice";
 import { jwtDecode } from "jwt-decode";
 import { PG } from "@/app/components/common/enums/PG";
 
-const SERVER = 'http://localhost:8080'
+const SERVER = "http://localhost:8080";
 
 export default function LoginPage() {
-  const router = useRouter()
-  const dispatch = useDispatch()
-  const auth = useSelector(getAuth)
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const auth = useSelector(getAuth);
   const formRef = useRef<HTMLInputElement>(null);
 
-  const [user, setUser] = useState({} as IUser) //하나의 instance로 만듬
-  const [isWrongId, setIsWrongId] = useState(false)
-  const [isTrueId, setIsTrueId] = useState(false)
-  const [beforeSubmit, setBeforeSubmit] = useState(true)
-  const [len, setLen] = useState('')
-  const [isWrongPw, setIsWrongPw] = useState(false)
-  const existsUsernameSelector = useSelector(getExistsUsername)
+  const [user, setUser] = useState({} as IUser); //하나의 instance로 만듬
+  const [isWrongId, setIsWrongId] = useState(false);
+  const [isTrueId, setIsTrueId] = useState(false);
+  const [beforeSubmit, setBeforeSubmit] = useState(true);
+  const [len, setLen] = useState("");
+  const [isWrongPw, setIsWrongPw] = useState(false);
+  const existsUsernameSelector = useSelector(getExistsUsername);
 
   //boolean type의 경우 is를 넣어줌
   const handleUsername = (e: any) => {
     const ID_CHECK = /^[a-zA-Z][a-zA-Z0-9]{4,10}$/g;
     //영어 대소문자로 시작하는 6~10자의 영어 대소문자 또는 숫자
-    setLen(e.target.value)
-    setBeforeSubmit(true)
+    setLen(e.target.value);
+    setBeforeSubmit(true);
 
     if (ID_CHECK.test(len)) {
-      setIsWrongId(false)
-      setIsTrueId(true)
+      setIsWrongId(false);
+      setIsTrueId(true);
       setUser({
         ...user,
-        username: e.target.value
-      })
-      console.log('ID_CHECK 내용 : ' + JSON.stringify(user))
+        username: e.target.value,
+      });
+      console.log("ID_CHECK 내용 : " + JSON.stringify(user));
     } else {
-      setIsWrongId(true)
-      setIsTrueId(false)
-
+      setIsWrongId(true);
+      setIsTrueId(false);
     }
-  }
+  };
   const handlePassword = (e: any) => {
     setUser({
       ...user,
-      password: e.target.value})
-  }
+      password: e.target.value,
+    });
+  };
 
-  
-const handleSubmit = () => {
-  console.log('user ...' + JSON.stringify(user))
-  dispatch(existsUsername(user.username))
+  const handleSubmit = () => {
+    console.log("user ..." + JSON.stringify(user));
+    dispatch(existsUsername(user.username))
       .then((res: any) => {
-        console.log(res.payload)
-          if (res.payload === true) {
-              dispatch(login(user))
-                  .then((resp: any) => {
-                      console.log('서버에서 넘어온 RES' + JSON.stringify(resp))
-                      console.log('서버에서 넘어온 메시지 : '+ resp.payload.message)
-                      console.log('서버에서 넘어온 토큰 : ' + resp.payload.accessToken)
-                      setCookie({}, 'message', resp.payload.message, { httpOnly: false, path: '/' })
-                      setCookie({}, 'accessToken', resp.payload.accessToken, { httpOnly: false, path: '/' })
-                      console.log('서버에서 넘어온 메시지 ' + parseCookies().message)
-                      console.log('서버에서 넘어온 토큰 ' + parseCookies().accessToken)
-                      console.log('토큰을 디코딩한 내용: ')
-                      jwtDecode<any>(parseCookies().accessToken)
-                      router.push(`${PG.USER}/detail/${1}`)
-                  })
-                  .catch((err: any) => {
-                      console.log('LOGIN FAIL')
-                  })
-          }
-          else {
-              console.log('아이디가 존재하지 않습니다')
-              setBeforeSubmit(false)
-              setIsWrongId(false)
-              setIsTrueId(false)
-          }
+        console.log(res.payload);
+        if (res.payload === true) {
+          dispatch(login(user))
+            .then((resp: any) => {
+              console.log("서버에서 넘어온 RES" + JSON.stringify(resp));
+              console.log("서버에서 넘어온 메시지 : " + resp.payload.message);
+              console.log("서버에서 넘어온 토큰 : " + resp.payload.accessToken);
+              setCookie({}, "message", resp.payload.message, {
+                httpOnly: false,
+                path: "/",
+              });
+              setCookie({}, "accessToken", resp.payload.accessToken, {
+                httpOnly: false,
+                path: "/",
+              });
+              console.log("서버에서 넘어온 메시지 " + parseCookies().message);
+              console.log("서버에서 넘어온 토큰 " + parseCookies().accessToken);
+              console.log("토큰을 디코딩한 내용: ");
+              jwtDecode<any>(parseCookies().accessToken);
+              router.push(`/`);
+            })
+            .catch((err: any) => {
+              console.log("Login Failed");
+            });
+        } else {
+          console.log("아이디가 존재하지 않습니다");
+          setBeforeSubmit(false);
+          setIsWrongId(false);
+          setIsTrueId(false);
+        }
       })
       .catch((err: any) => {
-        console.log('catch 로직 err 발생 : '+ `${err}`)
+        console.log("catch 로직 err 발생 : " + `${err}`);
       })
       .finally(() => {
-          console.log('최종적으로 반드시 수행되는 로직')
-      })
-  // dispatch(login(user))
-  setBeforeSubmit(false)
-  setIsWrongId(false)
-  setIsTrueId(false)
-  if (formRef.current) {
+        console.log("최종적으로 반드시 수행되는 로직");
+      });
+    // dispatch(login(user))
+    setBeforeSubmit(false);
+    setIsWrongId(false);
+    setIsTrueId(false);
+    if (formRef.current) {
       formRef.current.value = "";
-  }
-}
+    }
+  };
 
   return (
-    <div className="flex items-center justify-center h-screen w-full px-5 sm:px-0">
-      <div className="flex bg-white rounded-lg shadow-lg border overflow-hidden max-w-sm lg:max-w-4xl w-full">
-        <div className="w-full p-8 lg:w-1/2">
-          <p className="text-xl text-gray-600 text-center">Welcome back!</p>
-          <div className="mt-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Username alamblot0
+    <div className="flex justify-center h-screen w-full px-5 sm:px-0">
+      <div className="mt-20 w-[73vh] h-[67vh] flex bg-white  rounded-[3.5vh] shadow-2xl overflow-hidden">
+        <div className="w-full p-[8.5vh] justify-center items-center">
+          <p className="text-2xl text-gray-600 text-center">Sign in</p>
+          <div className="mt-10">
+            <label className="block text-gray-700 text-sm mb-2">
+              Username
             </label>
             <input
               onChange={handleUsername}
-              className="text-gray-700 border border-gray-300 rounded py-2 px-4 block w-full focus:outline-2 focus:outline-blue-700"
+              className="h-[6vh] text-gray-700 border border-gray-300 rounded-2xl py-2 px-4 block w-full focus:outline-2 focus:outline-blue-500"
               type="username"
               required
             />
           </div>
-          {isWrongId && len?.length > 1 && (<pre>
-            <h6 className='text-red-500' >
-              아이디 형식이 올바르지 않습니다.
-            </h6>
-          </pre>)}
-          {isTrueId && len?.length > 1 &&  (<pre>
-            <h6 className='text-blue-500' >
-              올바른 아이디입니다.
-            </h6>
-          </pre>)}
-          {!beforeSubmit && !existsUsernameSelector && <pre>
-            <h6 className='text-red-500' >
-              아이디가 존재하지 않습니다.
-            </h6>
-          </pre>}
-          <div className="mt-4 flex flex-col justify-between">
-            <div className="flex justify-between">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Password sV1@95
-              </label>
-            </div>
-            <input ref={formRef}
+          {isWrongId && len?.length > 1 && (
+            <pre>
+              <h6>Incorrect ID format.</h6>
+            </pre>
+          )}
+          {isTrueId && len?.length > 1 && (
+            <pre>
+              <h6>Correct ID format.</h6>
+            </pre>
+          )}
+          {!beforeSubmit && !existsUsernameSelector && (
+            <pre>
+              <h6>Username doesn't exist.</h6>
+            </pre>
+          )}
+          <div className="mt-4">
+            <label className="block text-gray-700 text-sm mb-2">
+              Password
+            </label>
+            <input
+              ref={formRef}
               onChange={handlePassword}
-              className="text-gray-700 border border-gray-300 rounded py-2 px-4 block w-full focus:outline-2 focus:outline-blue-700"
+              className="h-[6vh] text-gray-700 border border-gray-300 rounded-2xl py-2 px-4 block w-full focus:outline-2 focus:outline-blue-500"
               type="password"
             />
-            <a
-              href="#"
-              className="text-xs text-gray-500 hover:text-gray-900 text-end w-full mt-2"
-            >
-              Forget Password?
-            </a>
           </div>
-          {isWrongPw && (<pre>
-            <h6 className='text-red-500' >
-              잘못된 비밀번호 입니다.
-            </h6>
-          </pre>)}
-          <div className="mt-8">
-            <button onClick={handleSubmit}
-            className="static bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">
+          {isWrongPw && (
+            <pre>
+              <h6>Incorrect password. Try again.</h6>
+            </pre>
+          )}
+          <div className="flex justify-center">
+            <button
+              onClick={handleSubmit}
+              className="static m-11 text-white shadow-md hover:bg-gray-100 h-11 bg-black w-36 rounded-3xl">
               Login
             </button>
           </div>
-          <a
+          {/* <a
             href="#"
             className=" flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100"
           >
@@ -191,18 +195,23 @@ const handleSubmit = () => {
                 </h1>
               </div>
             </div>
-          </a>
-          <div className="mt-4 flex items-center w-full text-center">
+          </a> */}
+          <div className="w-full text-center items-end">
+            <Link
+              href="#"
+              className="text-xs text-blue-500 hover:text-gray-900 w-full mt-2"
+            >
+              Forget Password?
+            </Link><br />
             <Link
               href="/pages/users/join"
-              className="text-xs text-gray-500 capitalize text-center w-full"
+              className="text-xs text-gray-500 text-center w-full"
             >
-              Don&apos;t have any account yet?
-              <span className="text-blue-700"> Sign Up</span>
+              <span className="text-blue-500">Create accout</span>
             </Link>
-          </div>
+            </div>
         </div>
       </div>
     </div>
   );
-};
+}
