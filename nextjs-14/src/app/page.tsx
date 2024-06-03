102141b313793f76c95183a4babb0323282c70ed
GiselePark <hanied04@gmail.com>
2024-06-03 22:18:53 +0900

    Refactor

--- nextjs-14/src/app/page.tsx
+++ nextjs-14/src/app/page.tsx
@@ -1,22 +1,22 @@
 "use client";
-import { useForm, SubmitHandler } from "react-hook-form"
+import { useForm, SubmitHandler } from "react-hook-form";
 import Image from "next/image";
 import { Home } from "./components/common/style/header";
 import "./styles.css";
 import { useState, useEffect } from "react";
 
 type Inputs = {
-  question: string
-  exampleRequired?: string
-}
+  question: string;
+  exampleRequired?: string;
+};
 
 const variants = {
   open: { opacity: 1, x: 0 },
   closed: { opacity: 0, x: "-100%" },
-}
+};
 
 export default function HomePage() {
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState("");
   const [isOpen, setIsOpen] = useState(false);
 
   const {
@@ -24,61 +24,65 @@ export default function HomePage() {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Inputs>()
+  } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log('input content : '+JSON.stringify(data))
-    fetch('http://localhost:8000/api', {
+    console.log("input content : " + JSON.stringify(data));
+    fetch("http://localhost:8000/api", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     })
-    .then((response) => response.json())
-    .then((data) => setMessage(data.answer))
-    .catch((error) => console.log("error:", error));
-  }
-  console.log(watch("question")) //모니터링, 상태 값이 변하면 찍어줌
+      .then((response) => response.json())
+      .then((data) => setMessage(data.answer))
+      .catch((error) => console.log("error:", error));
+  };
+  console.log(watch("question")); //모니터링, 상태 값이 변하면 찍어줌
 
-  return (<>
+  return (
+    <>
       <div>
-      </div>
-      <Home className="h-[5vh] px-5 lg:px-0"></Home>
-        <div className="items-center flex justify-center px-5 lg:px-0"></div>
-          <div className="h-[70vh] items-center flex justify-center px-5 lg:px-0">
-            <div className="flex flex-col items-center justify-center w-full text-2xl xl:text-4xl font-extrabold text-900">
-              <h1 className=" dark:text-white"> Checkmate, Lawmate! </h1>
-            </div></div>
-            <div className="mx-auto max-w-[100vh] flex items-center gap-4">
-              <h4>{message? message:""}</h4>
-              <form onSubmit={handleSubmit(onSubmit)}>
-              <div>
-              <input
-              type="text"
-              {...register("question", { required: true })} 
-                className="w-[80vh] px-5 py-3 rounded-lg font-medium text-black bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                placeholder="Message here"
-              />
-              <button type="submit"
-              className="h-[5.4vh] w-[15vh] ml-4 static text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-                Send
-              </button>
+        <div>
+          <video
+            className="w-full font-semibold max-h-[80vh] object-cover pointer-events-none"
+            autoPlay
+            muted
+            loop
+            playsInline
+          >
+            <source src="/img/law.mp4" type="video/mp4" />
+          </video>
+          <div className="video-overlay">
+            <h1>
+              지금, 로메이트에게 물어보세요. <br />
+              Your Legal Solution, One Message Away.
+            </h1>
+          </div>
+        </div>
+        <div className="fixed bottom-0 h-[20vh] z-50 rounded-t-2xl transition-all duration-300 p-3 w-full bg-white dark:bg-black">
+          <div className="mx-auto max-w-[100vh] flex items-center gap-4">
+            <h4>{message ? message : ""}</h4>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="mt-2 justify-between">
+                <input
+                  type="text"
+                  {...register("question", { required: true })}
+                  className="flex-grow w-[80vh] px-5 py-3 rounded-3xl font-medium text-black bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                  placeholder="Message here"
+                />
+                <button
+                  type="submit"
+                  className="sticky h-[5.4vh] w-[15vh] ml-4 text-sm font-medium text-white dark:text-black rounded-3xl group shadow-md bg-black dark:bg-white "
+                >
+                  Send
+                </button>
               </div>
-              </form>
-              {errors.question && <span>This field is required</span>}
-            </div>
-            </>
+            </form>
+            {errors.question && <span>This field is required</span>}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
-
-
-
-
-
-
-
-
-
-
-    
-
