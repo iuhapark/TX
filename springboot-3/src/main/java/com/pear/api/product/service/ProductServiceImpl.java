9ec7d9bb2c80e5a5a115c0c26fad745fa2a2c406
GiselePark <hanied04@gmail.com>
2024-06-05 18:05:41 +0900

    Feat - CRUD implements logic

--- springboot-3/src/main/java/com/pear/api/product/service/ProductServiceImpl.java
+++ springboot-3/src/main/java/com/pear/api/product/service/ProductServiceImpl.java
@@ -1,18 +1,21 @@
 package com.pear.api.product.service;
 
 import com.pear.api.common.component.Messenger;
+import com.pear.api.product.model.Product;
 import com.pear.api.product.model.ProductDto;
 import com.pear.api.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
+
     @Override
     public Messenger save(ProductDto productDto) {
         entityToDto((productRepository.save(dtoToEntity(productDto))));
@@ -21,14 +24,29 @@ public class ProductServiceImpl implements ProductService{
                 .build();
     }
 
+    @Transactional
     @Override
     public Messenger deleteById(Long id) {
-        return null;
+        productRepository.deleteById(id);
+        return Messenger.builder()
+                .message(productRepository.findById(id).isPresent() ? "FAILURE" : "SUCCESS")
+                .build();
     }
 
     @Override
     public Messenger modify(ProductDto productDto) {
-        return null;
+        Optional<Product> optionalProduct = productRepository.findById(productDto.getId());
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            Product modifyProduct = product.toBuilder()
+                    .itemName(productDto.getItemName())
+                    .price(productDto.getPrice())
+                    .build();
+            productRepository.save(modifyProduct);
+            return Messenger.builder().message("SUCCESS").build();
+        } else {
+            return Messenger.builder().message("FAILURE").build();
+        }
     }
 
     @Override
