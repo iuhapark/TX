8d212b372b27de0153101196d0c0208f930d88d0
GiselePark <hanied04@gmail.com>
2024-06-05 18:10:52 +0900

    Feat - decoded JWT token

--- nextjs-14/src/app/atoms/menu/Menu.tsx
+++ nextjs-14/src/app/atoms/menu/Menu.tsx
@@ -1,18 +1,42 @@
-"use client";
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 import { PG } from "@/app/components/common/enums/PG";
-import { getUserById } from "@/app/components/user/service/user-slice";
-import { IUser } from "@/app/components/user/model/user";
-import { findUserById } from "@/app/components/user/service/user-service";
 import { parseCookies } from "nookies";
-import { IPayment } from "@/app/components/payment/model/payment";
-import { getPaymentById } from "@/app/components/payment/service/payment-slice";
+import { jwtDecode } from "jwt-decode";
 
 export function Menu() {
+  const [userId, setUserId] = useState<string | null>(null);
+
+  useEffect(() => {
+    const token = parseCookies().accessToken; // 토큰 가져오기
+
+    const processToken = () => {
+      if (!token) {
+        console.log("Token is missing");
+        return null;
+      }
+
+      let decoded: any;
+      try {
+        decoded = jwtDecode(token);
+        console.log("User id in Profile:", decoded.id);
+        return decoded.id;
+      } catch (error) {
+        console.error("Invalid token:", error);
+        return null;
+      }
+    };
+
+    const userIdFromToken = processToken(); // 함수 실행
+    if (userIdFromToken) {
+      setUserId(userIdFromToken); // userId 상태 업데이트
+    }
+  }, []); // 빈 종속성 배열로 컴포넌트가 마운트될 때 한 번만 실행됨
+
+  const paymentUrl = userId ? `${PG.PAY}/${userId}` : `${PG.PAY}/undefined`;
+  const itemtUrl = userId ? `${PG.ITEM}/${userId}` : `${PG.ITEM}/undefined`;
+
   return (
     <nav className="menu">
-
       <ul>
         <li>
           <a href={`${PG.BOARD}/list`}>변호사</a>
@@ -24,10 +48,10 @@ export function Menu() {
           <a href={`${PG.BOARD}/list`}>뉴스</a>
         </li>
         <li>
-          <a href={`${PG.PAY}/${1}`}>결제</a>
+          <a href={paymentUrl}>결제</a>
         </li>
         <li>
-          <a href={`${PG.ITEM}/${1}`}>상담</a>
+          <a href={itemtUrl}>상담</a>
         </li>
         <li>
           <a href={`${PG.BOARD}/list`}>게시판</a>
