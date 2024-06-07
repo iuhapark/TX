8a908b86ce2861a9d154ec69c9f47f5cf18aad9b
GiselePark <hanied04@gmail.com>
2024-06-07 18:30:58 +0900

    Design - modules

---
+++ nextjs-14/src/app/components/common/module/menu.tsx
@@ -0,0 +1,78 @@
+
+import "@/app/styles.css";
+import { useState, useEffect } from "react";
+import { useAnimate, stagger } from "framer-motion";
+import { Menu } from "@/app/atoms/menu/Menu";
+import { MenuBar } from "@/app/atoms/toggle/MenuBar";
+
+
+function useMenuAnimation(isOpen: boolean) {
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    const menuAnimations = isOpen
+      ? [
+          [
+            "nav",
+            { transform: "translateX(0%)" },
+            { ease: [0.08, 0.65, 0.53, 0.96], duration: 0.6 }
+          ],
+          [
+            "li",
+            { transform: "scale(1)", opacity: 1, filter: "blur(0px)" },
+            { delay: stagger(0.05), at: "-0.1" }
+          ]
+        ]
+      : [
+          [
+            "li",
+            { transform: "scale(0.5)", opacity: 0, filter: "blur(10px)" },
+            { delay: stagger(0.05, { from: "last" }), at: "<" }
+          ],
+          ["nav", { transform: "translateX(-100%)" }, { at: "-0.1" }]
+        ];
+
+    animate([
+      [
+        "path.top",
+        { d: isOpen ? "M 3 16.5 L 17 2.5" : "M 2 2.5 L 20 2.5" },
+        { at: "<" }
+      ],
+      ["path.middle", { opacity: isOpen ? 0 : 1 }, { at: "<" }],
+      [
+        "path.bottom",
+        { d: isOpen ? "M 3 2.5 L 17 16.346" : "M 2 16.346 L 20 16.346" },
+        { at: "<" }
+      ],
+      ...menuAnimations
+    ]);
+    
+    if (isOpen) {
+      document.body.style.paddingLeft = "220px";
+    } else {
+      document.body.style.paddingLeft = "0";
+    }
+
+    // Cleanup on unmount
+    return () => {
+      document.body.style.paddingLeft = "0";
+    };
+  }, [isOpen]);
+
+  return scope;
+}
+
+
+export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const scope = useMenuAnimation(isOpen);
+
+  return (
+    <div>
+    <div ref={scope}>
+      <Menu />
+      <MenuBar bar={() => setIsOpen(!isOpen)} />
+    </div>
+    </div>
+  );
+}
---
+++ nextjs-14/src/app/components/common/module/profile.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+const SERVER = "http://localhost:8080";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import { destroyCookie, parseCookies } from "nookies";
+import { useDispatch, useSelector } from "react-redux";
+import { findUserById, logout } from "../../user/service/user-service";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { getUserById } from "../../user/service/user-slice";
+import { IUser } from "../../user/model/user";
+import { PG } from "../enums/PG";
+import { jwtDecode } from "jwt-decode";
+import PaymentIcon from '@mui/icons-material/Payment';
+
+export default function Profile() {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const [showProfile, setShowProfile] = useState(false);
+  const user: IUser = useSelector(getUserById);
+  const token = parseCookies().accessToken;
+
+  useEffect(() => {
+    if (parseCookies().accessToken !== "") {
+      setShowProfile(true);
+    } else {
+      setShowProfile(false);
+    }
+  }, [parseCookies().accessToken]);
+
+  const profileHandler = () => {
+    const token = parseCookies().accessToken;
+    console.log("Token in Profile:", token);
+    if (token) {
+      try {
+        const decoded: any = jwtDecode(token);
+        console.log("User id in Profile:", decoded.id);
+        dispatch(findUserById(decoded.id));
+        router.push(`${PG.USER}${PG.DETAIL}/${decoded.id}`);
+      } catch (error) {
+        console.error("Invalid token:", error);
+        router.push(`${PG.USER}/login`);
+      }
+    } else {
+      router.push(`${PG.USER}/login`);
+    }
+  };
+
+  const logoutHandler = () => {
+    console.log("Before logout:", parseCookies().accessToken);
+    dispatch(logout())
+      .then((res: any) => {
+        destroyCookie(null, "accessToken");
+        setShowProfile(false);
+        router.push("/");
+        console.log("After logout:", parseCookies().accessToken);
+      })
+      .catch((err: any) => {
+        console.log("Error occurred when executing logout.: " + err);
+      });
+  };
+
+  return (
+    <nav className="fixed top-lg lg:py-[4rem] right-2">
+      <div>
+        {!showProfile && (
+          <PermIdentityIcon
+            type="button"
+            className="h-8 w-8 text-black dark:text-white cursor-pointer mr-4"
+            id="user-menu-button"
+            aria-expanded="false"
+            data-dropdown-toggle="user-dropdown"
+            data-dropdown-placement="bottom"
+            onClick={profileHandler}
+          />
+        )}
+        {showProfile && (
+          <div className="flex items-center">
+            <PermIdentityIcon
+              onClick={profileHandler}
+              className="mr-4 text-black dark:text-white cursor-pointer"
+            ></PermIdentityIcon>
+            <PaymentIcon 
+              onClick={() => router.push(`${PG.PAY}/${user.id}`)}
+              className="mr-4 text-black dark:text-white cursor-pointer"
+            />
+            {token && (
+              <LogoutIcon
+                onClick={logoutHandler}
+                className="h-8 w-8 text-black dark:text-white cursor-pointer"
+              >
+                <span className="sr-only">Logout</span>
+              </LogoutIcon>
+              
+            )}
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+}
