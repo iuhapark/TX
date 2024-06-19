0f51e50be50a1bc6cf91c8e3439c75467d65bf3a
iuhapark <hanied04@gmail.com>
2024-06-19 13:13:47 +0900

    Design - change style, layout

--- nextjs-14/src/app/components/common/module/footer.tsx
+++ nextjs-14/src/app/components/common/module/footer.tsx
@@ -1,18 +1,15 @@
 "use client";
 
 function Footer() {
-
   return (
-    <nav className="fixed bottom-0 right-0 z-50 rounded-b-2xl transition-all duration-300 p-3 w-full bg-white dark:bg-black">
-      <div className="text-right">
-        <span
-          className="text-[11px] text-gray-500 dark:text-white"
-        >
-          Copyright © 2024. JuhaPark. All rights reserved.
+    <nav className="fixed bottom-0 right-0 z-50 rounded-t-2xl transition-all duration-300 p-3 w-full bg-white dark:bg-black">
+      <div className="text-center">
+        <span className="text-[11px] text-gray-500 dark:text-white">
+          About Lawmate • Terms of Service • Privacy Policy <br />
+          Copyright © 2024 JuhaPark. All rights reserved
         </span>
       </div>
     </nav>
-
   );
 }
 
